5deb16333a31294ef7e1211a3bfdf2b5cda46362
adam_laptop <agillfi@ncsu.edu>
2014-03-07 20:00:05 -0500

    added event page to list all events

--- NSA/views.py
+++ NSA/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from NSA.models import Event
 
 
 def index(request):
@@ -31,6 +32,13 @@ def map(request):
 def event(request):
     context = RequestContext(request)
 
-    context_dict = {}
+    event_list = get_event_list()
+    context_dict = {'event_list': event_list}
 
     return render_to_response('nsa/event.html', context_dict, context)
+
+
+def get_event_list():
+    event_list = Event.objects.all()
+
+    return event_list
