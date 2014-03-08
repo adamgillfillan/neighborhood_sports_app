81b7b9e363ddd84c80c22ce782d5143b662c9091
adam_laptop <agillfi@ncsu.edu>
2014-03-07 23:34:13 -0500

    added admin display

--- NSA/admin.py
+++ NSA/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from NSA.models import Event
 
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('sport', 'players', 'info', 'address')
+
 # Register your models here.
 admin.site.register(Event)
-#test
