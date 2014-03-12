5a7e01a60b6c207229cdd2d7d4446f50ab6ddda9
adam_laptop <agillfi@ncsu.edu>
2014-03-11 20:48:01 -0400

    added hidden lat and long fields to form

--- NSA/forms.py
+++ NSA/forms.py
@@ -10,10 +10,8 @@ class EventForm(forms.ModelForm):
     info = forms.CharField(widget=forms.Textarea, help_text="Please enter any information that may help those "
                                                             "wishing to play with you.")
     address = forms.CharField(help_text="Please enter the address of where you are playing.")
-    #latitude = forms.FloatField(widget=forms.HiddenInput(), initial=0)
-    #longitude = forms.FloatField(widget=forms.HiddenInput(), initial=0)
+    latitude = forms.FloatField(widget=forms.HiddenInput(), initial=0)
+    longitude = forms.FloatField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Event
-
-        fields = ('sport', 'players', 'info', 'address')
